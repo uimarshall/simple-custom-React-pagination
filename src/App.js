d983750a0ec9c5b39b8979dbcf8494baefec0c5e
Marshall Akpan <uimarshall@gmail.com>
2022-06-24 16:38:40 +0100

    Display posts

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import { useState, useEffect } from 'react';
 import axios from 'axios';
 import './App.css';
+import Posts from './components/Posts';
 
 function App() {
   const [posts, setPosts] = useState([]);
@@ -18,13 +19,13 @@ function App() {
 
     fetchPosts();
   }, []); // The useEffect will only run once bc oc he empty dependency array
-
+  console.log([1, 2, 3, 4, 5, 6].slice(0, 5));
   console.log(posts);
 
   // Get current posts
-  const indexOfLastPost = currentPage * postsPerPage;
-  const indexOfFirstPost = indexOfLastPost - postsPerPage;
-  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost);
+  const indexOfLastPost = currentPage * postsPerPage; // 1*10 = 10, this is useful in the slicing.
+  const indexOfFirstPost = indexOfLastPost - postsPerPage; //10-10 = 0
+  const currentPosts = posts.slice(indexOfFirstPost, indexOfLastPost); //(0,10) slice from 0 but not including 10.,it implies 10 posts.
 
   // Change page
   const paginate = (pageNumber) => setCurrentPage(pageNumber);
@@ -32,6 +33,7 @@ function App() {
   return (
     <div className="App">
       <h1 className="text-primary mb-3">My Blog Posts</h1>
+      <Posts posts={currentPosts} loading={loading} />
     </div>
   );
 }
